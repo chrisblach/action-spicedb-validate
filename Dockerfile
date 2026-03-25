e60331bafb8c012de69e03510ff039483e90ffba
Chris Blachnio <chris.blachnio@crowdriff.com>
2026-03-25 14:41:52 -0400

    Update Dockerfile to use specific zed version and clean up entrypoint script

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 ARG ZED_VERSION=latest
-FROM quay.io/authzed/zed:${ZED_VERSION} AS zed
+FROM quay.io/authzed/zed:v0.35.0 AS zed
 
 FROM ubuntu:22.04
 COPY --from=zed /usr/local/bin/zed /zed
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-ZED_VERSION="${INPUT_ZED_VERSION:-latest}"
-export ZED_VERSION
-
 ARGS=""
 [[ "${INPUT_FAIL_ON_WARN:-}" == "true" ]] && ARGS+=" --fail-on-warn"
 
